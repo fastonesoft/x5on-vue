cbdce32a326ba650018daf0477d2b389ae472393
fastonesoft <2347632@qq.com>
2019-08-10 17:50:39 +0800

    8.10

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -3,7 +3,7 @@
         <template>
             <Row :gutter="16">
                 <i-col span="6">
-                    <Card shadow title="采集总数">
+                    <Card title="采集总数">
                         <!--数据采集完毕的数据 confirmed-->
                         <Tag color="green" slot="extra">数</Tag>
                         <div class="data-collect">{{ collect.num }}</div>
@@ -12,7 +12,7 @@
                     </Card>
                 </i-col>
                 <i-col span="6">
-                    <Card shadow title="税率测算">
+                    <Card title="税率测算">
                         <Tooltip content="税率测算说明" slot="extra" placement="top" transfer>
                             <Icon type="ios-alert-outline" size="18" />
                         </Tooltip>
@@ -22,7 +22,7 @@
                     </Card>
                 </i-col>
                 <i-col span="6">
-                    <Card shadow title="协作成果">
+                    <Card title="协作成果">
                         <Tooltip content="协作成果说明" slot="extra" placement="top" transfer>
                             <Icon type="ios-alert-outline" size="18" />
                         </Tooltip>
@@ -32,10 +32,15 @@
                     </Card>
                 </i-col>
                 <i-col span="6">
-                    <Card shadow title="快捷操作">
+                    <Card title="快捷操作">
 
                     </Card>
                 </i-col>
+            </Row>
+            <Row style="margin-top: 16px">
+                <Card>
+                    <Table :columns="cols" :data="datas"></Table>
+                </Card>
             </Row>
         </template>
     </dev-article>
@@ -53,7 +58,27 @@
                 count: {
                     num: 60,
                     not: 10
-                }
+                },
+                cols: [
+                    {
+                        title: '名称',
+                        key: 'name'
+                    },
+                    {
+                        title: '测试',
+                        key: 'value'
+                    }
+                ],
+                datas: [
+                    {
+                        name: 'adfasdf',
+                        value: 'adfa12123123123'
+                    },
+                    {
+                        name: '第二行',
+                        value: '数据是什么东西'
+                    }
+                ]
             }
         }
     }
@@ -67,6 +92,8 @@
     .data-collect_not{
         /*font-weight: bold;*/
         /*text-align: right;*/
+        overflow: hidden;
+        white-space: nowrap;
     }
     .ivu-divider-horizontal{
         margin: 16px 0;
