1fe773d5c31ae74973b151eb3121a294caf5ee40
fastonesoft <2347632@qq.com>
2019-09-18 18:39:46 +0800

    src

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,7 +2,8 @@
   <dev-article>
     <Card class="home-body">
       <div class="home-items">
-        <router-link class="home-item hidden-nowrap" :to="menu.name" replace v-for="menu of homeMenus" v-bind:key="menu.id">
+        <router-link class="home-item hidden-nowrap" :to="menu.name" replace v-for="menu of homeMenus"
+                     v-bind:key="menu.id">
           <img class="image" :src="menu.src" alt=""/>
           <div class="text">{{menu.title}}</div>
         </router-link>
@@ -18,41 +19,13 @@
             homeMenus() {
                 let menus = this.$store.state.menus;
                 menus.forEach(menu => {
-                    menu.src = '/static/' + menu.id + '.png';
+                    menu.src = require('@/assets/home/' + menu.id + '.png')
                 });
                 return menus;
             }
         }
     }
 </script>
-
-<!--html:-->
-
-<!--<img ref="study" :src="studyDataPic">-->
-<!--1-->
-<!--script:-->
-
-<!--data: {-->
-	<!--return {-->
-		<!--studyDataPic: require('@/assets/images/school.png')-->
-	<!--}-->
-<!--}-->
-<!--1-->
-<!--2-->
-<!--3-->
-<!--4-->
-<!--5-->
-<!--更改图片地址：-->
-
-<!--this.studyDataPic = require('@/assets/images/load.gif')-->
-<!--1-->
-<!--注意：-->
-<!--1.必须使用require-->
-<!--因为动态添加src被当做静态资源处理了，没有进行编译，所以要加上require-->
-<!--2.require里面的地址，要使用单引号-->
-<!--————————————————-->
-<!--版权声明：本文为CSDN博主「红瓤西瓜」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。-->
-<!--原文链接：https://blog.csdn.net/tian_xuewu/article/details/97980899-->
 
 <style scoped>
 
