a6f7e0df3c75986b8eb43a4f87c711dd60004308
fastonesoft <2347632@qq.com>
2019-11-20 15:48:14 +0800

    allot.1

--- web/src/views/Allot.vue
+++ web/src/views/Allot.vue
@@ -35,12 +35,7 @@
           </Card>
         </i-col>
       </Row>
-      <Split
-        v-model="split1"
-        class="split margin-top8"
-        min="600"
-        max="350"
-      >
+      <Split v-model="split1" class="split margin-top8" min="600" max="350">
         <div slot="left" class="slot-left">
           <Tabs value="table">
             <TabPane label="任务标的" name="table">
@@ -95,7 +90,7 @@
             <TabPane label="任务分配" name="table">
               <div v-if="current">
                 <h2 style="text-align: center;">税务案件任务分配</h2>
-                <br>
+                <br />
                 <CellGroup>
                   <Cell title="税费测算">
                     <Select
@@ -103,14 +98,11 @@
                       v-model="examUser.count_user_id"
                       placeholder="人员选择..."
                       :disabled="current.count===1"
+					  @on-change="userChange(11)"
                       transfer
                       slot="extra"
                     >
-                      <Option
-                        v-for="item in users"
-                        :value="item.id"
-                        :key="item.id"
-                      >{{ item.name }}</Option>
+                      <Option v-for="item in users" :value="item.id" :key="item.id">{{ item.name }}</Option>
                     </Select>
                   </Cell>
                   <Cell title="测算复核">
@@ -122,14 +114,10 @@
                       transfer
                       slot="extra"
                     >
-                      <Option
-                        v-for="item in users"
-                        :value="item.id"
-                        :key="item.id"
-                      >{{ item.name }}</Option>
+                      <Option v-for="item in users" :value="item.id" :key="item.id">{{ item.name }}</Option>
                     </Select>
                   </Cell>
-                                    <div id="teamDom" class="mult-select multi-select-padding">案件审批</div>
+                  <div id="teamDom" class="mult-select multi-select-padding">案件审批</div>
                   <Cell title="文书制作">
                     <Select
                       class="multi-select"
@@ -139,14 +127,9 @@
                       transfer
                       slot="extra"
                     >
-                      <Option
-                        v-for="item in users"
-                        :value="item.id"
-                        :key="item.id"
-                      >{{ item.name }}</Option>
+                      <Option v-for="item in users" :value="item.id" :key="item.id">{{ item.name }}</Option>
                     </Select>
                   </Cell>
-
                 </CellGroup>
                 <Divider></Divider>
                 <h4 class="first-line">指定人员，则只能由被指定人员进行操作（要有对应岗位权限）；不指定人员，则由相关岗位人员执行操作。</h4>
@@ -155,7 +138,12 @@
             <!--表头附加相关操作：-->
             <template slot="extra">
               <Row class="hidden-nowrap">
-                <Button type="primary" size="small" @click="allotExe" v-if="current && !current.alloted">执行</Button>
+                <Button
+                  type="primary"
+                  size="small"
+                  @click="allotExe"
+                  v-if="current && !current.alloted"
+                >执行</Button>
               </Row>
             </template>
           </Tabs>
@@ -214,13 +202,14 @@ export default {
           key: "allot",
           align: "center",
           render: (h, params) => {
-            if (params.row.alloted) return h('Icon', {
-                      props: {
-                          type: 'ios-checkmark-circle-outline',
-                          size: '24',
-                          color: '#2d8cf0'
-                      },
-                  });
+            if (params.row.alloted)
+              return h("Icon", {
+                props: {
+                  type: "ios-checkmark-circle-outline",
+                  size: "24",
+                  color: "#2d8cf0"
+                }
+              });
           }
         },
         {
@@ -228,13 +217,14 @@ export default {
           key: "count",
           align: "center",
           render: (h, params) => {
-            if (params.row.count) return h('Icon', {
-                      props: {
-                          type: 'ios-checkmark-circle-outline',
-                          size: '24',
-                          color: '#2d8cf0'
-                      },
-                  });
+            if (params.row.count)
+              return h("Icon", {
+                props: {
+                  type: "ios-checkmark-circle-outline",
+                  size: "24",
+                  color: "#2d8cf0"
+                }
+              });
           }
         },
         {
@@ -242,13 +232,14 @@ export default {
           key: "counted",
           align: "center",
           render: (h, params) => {
-            if (params.row.counted) return h('Icon', {
-                      props: {
-                          type: 'ios-checkmark-circle-outline',
-                          size: '24',
-                          color: '#2d8cf0'
-                      },
-                  });
+            if (params.row.counted)
+              return h("Icon", {
+                props: {
+                  type: "ios-checkmark-circle-outline",
+                  size: "24",
+                  color: "#2d8cf0"
+                }
+              });
           }
         },
         {
@@ -256,13 +247,14 @@ export default {
           key: "teamed",
           align: "center",
           render: (h, params) => {
-            if (params.row.teamed) return h('Icon', {
-                      props: {
-                          type: 'ios-checkmark-circle-outline',
-                          size: '24',
-                          color: '#2d8cf0'
-                      },
-                  });
+            if (params.row.teamed)
+              return h("Icon", {
+                props: {
+                  type: "ios-checkmark-circle-outline",
+                  size: "24",
+                  color: "#2d8cf0"
+                }
+              });
           }
         },
         {
@@ -270,15 +262,16 @@ export default {
           key: "docued",
           align: "center",
           render: (h, params) => {
-            if (params.row.docued) return h('Icon', {
-                      props: {
-                          type: 'ios-checkmark-circle-outline',
-                          size: '24',
-                          color: '#2d8cf0'
-                      },
-                  });
+            if (params.row.docued)
+              return h("Icon", {
+                props: {
+                  type: "ios-checkmark-circle-outline",
+                  size: "24",
+                  color: "#2d8cf0"
+                }
+              });
           }
-        },
+        }
       ],
       ajaxs: [],
       pageIndex: 1,
@@ -320,7 +313,7 @@ export default {
 
       ajax_examUser: {},
 
-      teamDom: null,
+      teamDom: null
     };
   },
   methods: {
@@ -370,16 +363,10 @@ export default {
     },
     sizeChange(size) {
       this.pageSize = size;
-	},
-	
-	// 多选取值
-	selectValues() {
-		return this.teamDom.getValue('valueStr')
-	},
+    },
 
     // 表格选择，查询分配用户
     selectChange(row) {
-		this.selectValues();
       this.current = row;
       this.countLoading = true;
       // 查询任务分配用户
@@ -389,36 +376,49 @@ export default {
           this.countLoading = false;
 
           // 多选设置
-          let users = this.users.concat();
-          // 审批用户状态表
           if (xcon.isNotNull(res)) {
-            let user_ids = res.teamed_users ? res.teamed_users.split(',') : [];
-			let exameds = res.teamed_examed ? res.teamed_examed.split(',') : [];
+            let user_ids = res.teamed_users ? res.teamed_users.split(",") : [];
+            let exameds = res.teamed_examed ? res.teamed_examed.split(",") : [];
 
+            let users = this.users.concat();
             // 修改users列表状态
             users.forEach(item => {
               item.disabled = false;
               item.selected = false;
-              for (let i=0; i<user_ids.length; i++) {
+              for (let i = 0; i < user_ids.length; i++) {
                 if (item.id === user_ids[i]) {
                   item.selected = true;
-                  item.disabled = exameds[i]==='1';
+                  item.disabled = exameds[i] === "1";
                   break;
                 }
               }
-            })
+            });
+            // 清空，数据
+            this.teamDom.update({
+              data: []
+            });
+            // 重新加载，不然不渲染
+            this.teamDom.update({
+              data: users
+            });
+          } else {
+            this.teamDom.update({
+              data: []
+            });
           }
-          window.console.log(users)
-
-          this.teamDom.update({
-            data: users
-          })
         })
         .catch(error => {
           this.countLoading = false;
           this.$Message.error(error);
         });
-    },
+	},
+	
+	// 用户选择
+	userChange(value) {
+
+		window.console.log(value)
+window.console.log(dd)
+	},
 
     // 标的任务分配
     allotExe() {
@@ -441,7 +441,6 @@ export default {
         });
     },
 
-
     // 测算结束，提交复核
     countExam() {
       let row = this.current;
@@ -463,8 +462,7 @@ export default {
           this.countLoading = false;
           this.$Message.error(error);
         });
-    },
-
+    }
   },
   computed: {
     dataCount() {
@@ -513,11 +511,13 @@ export default {
       // 分配任务的用户对象
       if (xcon.isNotNull(this.ajax_examUser)) {
         let user = Object.assign({}, this.ajax_examUser);
-        user.teamed_user_id = user.teamed_users ? user.teamed_users.split(',') : [];
-        return user
+        user.teamed_user_id = user.teamed_users
+          ? user.teamed_users.split(",")
+          : [];
+        return user;
       }
-      return {}
-    },
+      return {};
+    }
   },
   created() {
     this.$.gets("/allot/index")
@@ -533,22 +533,22 @@ export default {
       });
   },
   mounted() {
-	  let that = this;
-      // 渲染多选
-      this.teamDom = xmSelect.render({
-		el: '#teamDom',
-		prop: {
-			value: 'id',
-		},
-        theme: {
-          color: '#2d8cf0'
-        },
-		data: [],
-		hide() {
-			let dd = that.selectValues();
-			window.console.log(dd);
-		}
-      })
+    let that = this;
+    // 渲染多选
+    this.teamDom = xmSelect.render({
+      el: "#teamDom",
+      prop: {
+        value: "id"
+      },
+      theme: {
+        color: "#2d8cf0"
+      },
+      data: [],
+      hide() {
+        let value = that.teamDom.getValue("valueStr");
+        window.console.log(value);
+      }
+    });
   }
 };
 </script>
